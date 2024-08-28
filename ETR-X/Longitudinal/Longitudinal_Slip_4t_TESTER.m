8b3ddb356e64c4f7aae8e7f7327275df37d0fda0
teretyres <teresa.martin.alguacil@estudiantat.upc.edu>
2024-08-28 18:08:13 +0200

    longitudinal tester

--- ETR-X/Longitudinal/Longitudinal_Slip_4t_TESTER.m
+++ ETR-X/Longitudinal/Longitudinal_Slip_4t_TESTER.m
@@ -50,202 +50,198 @@ LHX=1; %Scale factor of Fx horizontal shift; valor inventat
 LXA=1; %Scale factor of alpha influence on Fx; valor inventat
 
 %Data
-slip = 0:0.01:1.0; %vehicle slip
-VV = (0:5:90)/3.6; %Vehicle Velocity m/s
+slip = 0:0.01:1.0; %SENSOR vehicle slip
+VV = (0:1:100)/3.6; %SENSOR Vehicle Velocity m/s
 PI = 0.8; %pressio en bar
 DPI = (PI-0.97)/0.97;
+Acx=10:0.1:20; %SENSOR m/s^2
 
 
-
-
-for i = 1:length(slip)
-    for j = 1:length(VV)
+if length(slip)==length(VV)
+    for i = 1:length(slip)
+  
+        fzr(i) = W*cogx/l; % Rear axle load load
+        fzf(i) = W-fzr(i); % Front axle load load
+        V(i) = VV(i)*(slip(i)+1); % Wheel velocity m/s
+        k(i) = (V(i)-VV(i))/VV(i); %slip(i)
+        WTx(i) = (h/l)*m*Acx(i); %Longitudinal weight transfer N
+        WTF(i) = -WTx(i); 
+        WTR(i) = WTx(i);            
+        FZf(i) = fzf(i) + WTF(i);% Front axle load + weight transfer
+        FZr(i) = fzr(i) + WTR(i);% Rear axle load + weight transfer
+        FZFL(i) = FZf(i)/2; % Front left wheel load
+        FZFR(i) = FZf(i)/2; % Front right wheel load
+        FZRL(i) = FZr(i)/2; % Rear left wheel load
+        FZRR(i) = FZr(i)/2; % Rear right wheel load
+        DFZFL(i) = (FZFL(i)-1080)/1080; %load diferential FL
+        DFZFR(i) = (FZFR(i)-1080)/1080; %load diferential FR
+        DFZRL(i) = (FZRL(i)-1080)/1080; %load diferential RL
+        DFZRR(i) = (FZRR(i)-1080)/1080; %load diferential RR
         
-        Acx(i,j) = 0; %Acceleracio inicial en m/s
-        fzr(i,j) = W*cogx/l; % Rear axle load load
-        fzf(i,j) = W-fzr(i,j); % Front axle load load
+        
+        %FRONT LEFT
 
-        for k=1:100
+        %MF-Tyre 5.2, 6.0, 6.1
+        s1=1;
+        SHXFL(i)= (PHX1+PHX2*DFZFL(i))*LHX; 
 
-            V(i,j) = VV(j)*(slip(i)+1); % Wheel velocity m/s
-            k(i,j) = (V(i,j)-VV(j))/VV(j); %slip(i)
-            WTx(i,j) = (h/l)*m*Acx(i,j); %Longitudinal weight transfer N
-            WTF(i,j) = -WTx(i,j); 
-            WTR(i,j) = WTx(i,j);            
-            FZf(i,j) = fzf(i,j) + WTF(i,j);% Front axle load + weight transfer
-            FZr(i,j) = fzr(i,j) + WTR(i,j);% Rear axle load + weight transfer
-            FZFL(i,j) = FZf(i,j)/2; % Front left wheel load
-            FZFR(i,j) = FZf(i,j)/2; % Front right wheel load
-            FZRL(i,j) = FZr(i,j)/2; % Rear left wheel load
-            FZRR(i,j) = FZr(i,j)/2; % Rear right wheel load
-            DFZFL(i,j) = (FZFL(i,j)-1080)/1080; %load diferential FL
-            DFZFR(i,j) = (FZFR(i,j)-1080)/1080; %load diferential FR
-            DFZRL(i,j) = (FZRL(i,j)-1080)/1080; %load diferential RL
-            DFZRR(i,j) = (FZRR(i,j)-1080)/1080; %load diferential RR
+        %Pure Slip
+        kxFL(i) = k(i)+SHXFL(i);
+        CXFL(i) = PCX1*LCX;
+        UFL(i)  = (PDX1+PDX2*DFZFL(i))*(1+PPX3*DPI+PPX4*DPI^2)*(1-PDX3*CAM^2)*LMUX;
+        DXFL(i) = UFL(i)*FZFL(i)*s1;
+        ExFL(i) = (PEX1+PEX2*DFZFL(i)+PEX3*DFZFL(i)^2)*(1-PEX4*sign(kxFL(i)))*LEX;
+        KXkFL(i) = FZFL(i)*(PKX1+PKX2*DFZFL(i))*exp(PKX3*DFZFL(i))*(1+PPX1*DPI+PPX2*DPI^2)*LKXk; 
+        BXFL(i) = KXkFL(i)/(CXFL(i)*DXFL(i));
+        %Combined slip
+        %{
+        SHalfaFL= RHX1
+        alfaSFL= alfaF+SHalfaFL
+        BXalfaFL= (RBX1+RBX3*CAM^2)*cos(arctan(RBX2*k))*LXA
+        CXalfaFL= RCX1
+        EXAFL= REX1+REX2*DFZ
+        %}
+        GxalfaFL=1;
+        %{
+        GxalfaFL= (cos(CXalfaFL*arctan(BXalfaFL*alfaSFL-EXAFL(BXalfaFL*alfaSFL-arctan(BXalfaFL*alfaSFL)))))/(cos(CXalfaFL*arctan(BXalfaFL*SHalfaFL-EXalfa(BXalfaFL-SHalfaFL-arctan(BXalfaFL*SHXalfaFL)))))
+        when combined slip is not used: Gxa=1
+        %}
+        %Turn slip
+        s1=1; 
+        %{
+        BXP1FL= PDXP1(1+PDXP2*DFZ)*cos(arctan(PDXP3*k))
+        s1= cos(arctan(BXP1FL*R*PF))
+        %when turn slip not used: s1=1
+        %}
+        
+        FxFL(i)=(DXFL(i)*sin(CXFL(i)*atan(BXFL(i)*kxFL(i)-ExFL(i)*(BXFL(i)*kxFL(i)-atan(BXFL(i)*kxFL(i)))))+SVX)*GxalfaFL;
+        
+        
+        %FRONT RIGHT
+        
+        %MF-Tyre 5.2, 6.0, 6.1
+        s1=1;
+        SHXFR(i)= (PHX1+PHX2*DFZFR(i))*LHX; 
+        
+        %Pure Slip
+        kxFR(i) = k(i)+SHXFR(i);
+        CXFR(i) = PCX1*LCX;
+        UFR(i)  = (PDX1+PDX2*DFZFR(i))*(1+PPX3*DPI+PPX4*DPI^2)*(1-PDX3*CAM^2)*LMUX;
+        DXFR(i) = UFR(i)*FZFR(i)*s1;
+        ExFR(i) = (PEX1+PEX2*DFZFR(i)+PEX3*DFZFR(i)^2)*(1-PEX4*sign(kxFR(i)))*LEX;
+        KXkFR(i) = FZFR(i)*(PKX1+PKX2*DFZFR(i))*exp(PKX3*DFZFR(i))*(1+PPX1*DPI+PPX2*DPI^2)*LKXk; 
+        BXFR(i) = KXkFR(i)/(CXFR(i)*DXFR(i));
+        %Combined slip
+        %{
+        SHalfaFR= RHX1
+        alfaSFR= alfaF+SHalfaFR
+        BXalfaFR= (RBX1+RBX3*CAM^2)*cos(arctan(RBX2*k))*LXA
+        CXalfaFR= RCX1
+        EXAFR= REX1+REX2*DFZ
+        %}
+        GxalfaFR=1;
+        %{
+        GxalfaFR= (cos(CXalfaFR*arctan(BXalfaFR*alfaSFR-EXAFR(BXalfaFR*alfaSFR-arctan(BXalfaFR*alfaSFR)))))/(cos(CXalfaFR*arctan(BXalfaFR*SHalfaFR-EXalfa(BXalfaFR-SHalfaFR-arctan(BXalfaFR*SHXalfaFR)))))
+        when combined slip is not used: Gxa=1
+        %}
+        %Turn slip
+        s1=1; 
+        %{
+        BXP1FR= PDXP1(1+PDXP2*DFZ)*cos(arctan(PDXP3*k))
+        s1= cos(arctan(BXP1FR*R*PF))
+        %when turn slip not used: s1=1
+        %}
+        
+        FxFR(i)=(DXFR(i)*sin(CXFR(i)*atan(BXFR(i)*kxFR(i)-ExFR(i)*(BXFR(i)*kxFR(i)-atan(BXFR(i)*kxFR(i)))))+SVX)*GxalfaFR;
+        
+        
+        %REAR LEFT
+        
+        %MF-Tyre 5.2, 6.0, 6.1
+        s1=1;
+        SHXRL(i)= (PHX1+PHX2*DFZRL(i))*LHX; 
+        
+        %Pure Slip
+        kxRL(i) = k(i)+SHXRL(i);
+        CXRL(i) = PCX1*LCX;
+        URL(i)  = (PDX1+PDX2*DFZRL(i))*(1+PPX3*DPI+PPX4*DPI^2)*(1-PDX3*CAM^2)*LMUX;
+        DXRL(i) = URL(i)*FZRL(i)*s1;
+        ExRL(i) = (PEX1+PEX2*DFZRL(i)+PEX3*DFZRL(i)^2)*(1-PEX4*sign(kxRL(i)))*LEX;
+        KXkRL(i) = FZRL(i)*(PKX1+PKX2*DFZRL(i))*exp(PKX3*DFZRL(i))*(1+PPX1*DPI+PPX2*DPI^2)*LKXk; 
+        BXRL(i) = KXkRL(i)/(CXRL(i)*DXRL(i));
+        %Combined slip
+        %{
+        SHalfaRL= RHX1
+        alfaSRL= alfaF+SHalfaRL
+        BXalfaRL= (RBX1+RBX3*CAM^2)*cos(arctan(RBX2*k))*LXA
+        CXalfaRL= RCX1
+        EXARL= REX1+REX2*DFZ
+        %}
+        GxalfaRL=1;
+        %{
+        GxalfaRL= (cos(CXalfaRL*arctan(BXalfaRL*alfaSRL-EXARL(BXalfaRL*alfaSRL-arctan(BXalfaRL*alfaSRL)))))/(cos(CXalfaRL*arctan(BXalfaRL*SHalfaRL-EXalfa(BXalfaRL-SHalfaRL-arctan(BXalfaRL*SHXalfaRL)))))
+        when combined slip is not used: Gxa=1
+        %}
+        %Turn slip
+        s1=1; 
+        %{
+        BXP1RL= PDXP1(1+PDXP2*DFZ)*cos(arctan(PDXP3*k))
+        s1= cos(arctan(BXP1RL*R*PF))
+        %when turn slip not used: s1=1
+        %}
+        
+        FxRL(i)=(DXRL(i)*sin(CXRL(i)*atan(BXRL(i)*kxRL(i)-ExRL(i)*(BXRL(i)*kxRL(i)-atan(BXRL(i)*kxRL(i)))))+SVX)*GxalfaRL;
+        
+        
+        %REAR RIGHT
+        
+        %MF-Tyre 5.2, 6.0, 6.1
+        s1=1;
+        SHXRR(i)= (PHX1+PHX2*DFZRR(i))*LHX; 
+        
+        %Pure Slip
+        kxRR(i) = k(i)+SHXRR(i);
+        CXRR(i) = PCX1*LCX;
+        URR(i)  = (PDX1+PDX2*DFZRR(i))*(1+PPX3*DPI+PPX4*DPI^2)*(1-PDX3*CAM^2)*LMUX;
+        DXRR(i) = URR(i)*FZRR(i)*s1;
+        ExRR(i) = (PEX1+PEX2*DFZRR(i)+PEX3*DFZRR(i)^2)*(1-PEX4*sign(kxRR(i)))*LEX;
+        KXkRR(i) = FZRR(i)*(PKX1+PKX2*DFZRR(i))*exp(PKX3*DFZRR(i))*(1+PPX1*DPI+PPX2*DPI^2)*LKXk; 
+        BXRR(i) = KXkRR(i)/(CXRR(i)*DXRR(i));
+        %Combined slip
+        %{
+        SHalfaRR= RHX1
+        alfaSRR= alfaF+SHalfaRR
+        BXalfaRR= (RBX1+RBX3*CAM^2)*cos(arctan(RBX2*k))*LXA
+        CXalfaRR= RCX1
+        EXARR= REX1+REX2*DFZ
+        %}
+        GxalfaRR=1;
+        %{
+        GxalfaRR= (cos(CXalfaRR*arctan(BXalfaRR*alfaSRR-EXARR(BXalfaRR*alfaSRR-arctan(BXalfaRR*alfaSRR)))))/(cos(CXalfaRR*arctan(BXalfaRR*SHalfaRR-EXalfa(BXalfaRR-SHalfaRR-arctan(BXalfaRR*SHXalfaRR)))))
+        when combined slip is not used: Gxa=1
+        %}
+        %Turn slip
+        s1=1; 
+        %{
+        BXP1RR= PDXP1(1+PDXP2*DFZ)*cos(arctan(PDXP3*k))
+        s1= cos(arctan(BXP1RR*R*PF))
+        %when turn slip not used: s1=1
+        %}
+        
+        FxRR(i)=(DXRR(i)*sin(CXRR(i)*atan(BXRR(i)*kxRR(i)-ExRR(i)*(BXRR(i)*kxRR(i)-atan(BXRR(i)*kxRR(i)))))+SVX)*GxalfaRR;
+        
+        %Total
+        Fx4(i)=FxFL(i)+FxFR(i)+FxRL(i)+FxRR(i); %Only 4WD
+        Fx2(i)=FxRL(i)+FxRR(i); %2WD
+        Acx(i)=Fx2(i)/m; %Longitudinal acceleration
             
-            
-            %FRONT LEFT
-
-            %MF-Tyre 5.2, 6.0, 6.1
-            s1=1;
-            SHXFL(i,j)= (PHX1+PHX2*DFZFL(i,j))*LHX; 
-
-            %Pure Slip
-            kxFL(i,j) = k(i,j)+SHXFL(i,j);
-            CXFL(i,j) = PCX1*LCX;
-            UFL(i,j)  = (PDX1+PDX2*DFZFL(i,j))*(1+PPX3*DPI+PPX4*DPI^2)*(1-PDX3*CAM^2)*LMUX;
-            DXFL(i,j) = UFL(i,j)*FZFL(i,j)*s1;
-            ExFL(i,j) = (PEX1+PEX2*DFZFL(i,j)+PEX3*DFZFL(i,j)^2)*(1-PEX4*sign(kxFL(i,j)))*LEX;
-            KXkFL(i,j) = FZFL(i,j)*(PKX1+PKX2*DFZFL(i,j))*exp(PKX3*DFZFL(i,j))*(1+PPX1*DPI+PPX2*DPI^2)*LKXk; 
-            BXFL(i,j) = KXkFL(i,j)/(CXFL(i,j)*DXFL(i,j));
-            %Combined slip
-            %{
-            SHalfaFL= RHX1
-            alfaSFL= alfaF+SHalfaFL
-            BXalfaFL= (RBX1+RBX3*CAM^2)*cos(arctan(RBX2*k))*LXA
-            CXalfaFL= RCX1
-            EXAFL= REX1+REX2*DFZ
-            %}
-            GxalfaFL=1;
-            %{
-            GxalfaFL= (cos(CXalfaFL*arctan(BXalfaFL*alfaSFL-EXAFL(BXalfaFL*alfaSFL-arctan(BXalfaFL*alfaSFL)))))/(cos(CXalfaFL*arctan(BXalfaFL*SHalfaFL-EXalfa(BXalfaFL-SHalfaFL-arctan(BXalfaFL*SHXalfaFL)))))
-            when combined slip is not used: Gxa=1
-            %}
-            %Turn slip
-            s1=1; 
-            %{
-            BXP1FL= PDXP1(1+PDXP2*DFZ)*cos(arctan(PDXP3*k))
-            s1= cos(arctan(BXP1FL*R*PF))
-            %when turn slip not used: s1=1
-            %}
-            
-            FxFL(i,j)=(DXFL(i,j)*sin(CXFL(i,j)*atan(BXFL(i,j)*kxFL(i,j)-ExFL(i,j)*(BXFL(i,j)*kxFL(i,j)-atan(BXFL(i,j)*kxFL(i,j)))))+SVX)*GxalfaFL;
-            
-            
-            %FRONT RIGHT
-            
-            %MF-Tyre 5.2, 6.0, 6.1
-            s1=1;
-            SHXFR(i,j)= (PHX1+PHX2*DFZFR(i,j))*LHX; 
-            
-            %Pure Slip
-            kxFR(i,j) = k(i,j)+SHXFR(i,j);
-            CXFR(i,j) = PCX1*LCX;
-            UFR(i,j)  = (PDX1+PDX2*DFZFR(i,j))*(1+PPX3*DPI+PPX4*DPI^2)*(1-PDX3*CAM^2)*LMUX;
-            DXFR(i,j) = UFR(i,j)*FZFR(i,j)*s1;
-            ExFR(i,j) = (PEX1+PEX2*DFZFR(i,j)+PEX3*DFZFR(i,j)^2)*(1-PEX4*sign(kxFR(i,j)))*LEX;
-            KXkFR(i,j) = FZFR(i,j)*(PKX1+PKX2*DFZFR(i,j))*exp(PKX3*DFZFR(i,j))*(1+PPX1*DPI+PPX2*DPI^2)*LKXk; 
-            BXFR(i,j) = KXkFR(i,j)/(CXFR(i,j)*DXFR(i,j));
-            %Combined slip
-            %{
-            SHalfaFR= RHX1
-            alfaSFR= alfaF+SHalfaFR
-            BXalfaFR= (RBX1+RBX3*CAM^2)*cos(arctan(RBX2*k))*LXA
-            CXalfaFR= RCX1
-            EXAFR= REX1+REX2*DFZ
-            %}
-            GxalfaFR=1;
-            %{
-            GxalfaFR= (cos(CXalfaFR*arctan(BXalfaFR*alfaSFR-EXAFR(BXalfaFR*alfaSFR-arctan(BXalfaFR*alfaSFR)))))/(cos(CXalfaFR*arctan(BXalfaFR*SHalfaFR-EXalfa(BXalfaFR-SHalfaFR-arctan(BXalfaFR*SHXalfaFR)))))
-            when combined slip is not used: Gxa=1
-            %}
-            %Turn slip
-            s1=1; 
-            %{
-            BXP1FR= PDXP1(1+PDXP2*DFZ)*cos(arctan(PDXP3*k))
-            s1= cos(arctan(BXP1FR*R*PF))
-            %when turn slip not used: s1=1
-            %}
-            
-            FxFR(i,j)=(DXFR(i,j)*sin(CXFR(i,j)*atan(BXFR(i,j)*kxFR(i,j)-ExFR(i,j)*(BXFR(i,j)*kxFR(i,j)-atan(BXFR(i,j)*kxFR(i,j)))))+SVX)*GxalfaFR;
-            
-            
-            %REAR LEFT
-            
-            %MF-Tyre 5.2, 6.0, 6.1
-            s1=1;
-            SHXRL(i,j)= (PHX1+PHX2*DFZRL(i,j))*LHX; 
-            
-            %Pure Slip
-            kxRL(i,j) = k(i,j)+SHXRL(i,j);
-            CXRL(i,j) = PCX1*LCX;
-            URL(i,j)  = (PDX1+PDX2*DFZRL(i,j))*(1+PPX3*DPI+PPX4*DPI^2)*(1-PDX3*CAM^2)*LMUX;
-            DXRL(i,j) = URL(i,j)*FZRL(i,j)*s1;
-            ExRL(i,j) = (PEX1+PEX2*DFZRL(i,j)+PEX3*DFZRL(i,j)^2)*(1-PEX4*sign(kxRL(i,j)))*LEX;
-            KXkRL(i,j) = FZRL(i,j)*(PKX1+PKX2*DFZRL(i,j))*exp(PKX3*DFZRL(i,j))*(1+PPX1*DPI+PPX2*DPI^2)*LKXk; 
-            BXRL(i,j) = KXkRL(i,j)/(CXRL(i,j)*DXRL(i,j));
-            %Combined slip
-            %{
-            SHalfaRL= RHX1
-            alfaSRL= alfaF+SHalfaRL
-            BXalfaRL= (RBX1+RBX3*CAM^2)*cos(arctan(RBX2*k))*LXA
-            CXalfaRL= RCX1
-            EXARL= REX1+REX2*DFZ
-            %}
-            GxalfaRL=1;
-            %{
-            GxalfaRL= (cos(CXalfaRL*arctan(BXalfaRL*alfaSRL-EXARL(BXalfaRL*alfaSRL-arctan(BXalfaRL*alfaSRL)))))/(cos(CXalfaRL*arctan(BXalfaRL*SHalfaRL-EXalfa(BXalfaRL-SHalfaRL-arctan(BXalfaRL*SHXalfaRL)))))
-            when combined slip is not used: Gxa=1
-            %}
-            %Turn slip
-            s1=1; 
-            %{
-            BXP1RL= PDXP1(1+PDXP2*DFZ)*cos(arctan(PDXP3*k))
-            s1= cos(arctan(BXP1RL*R*PF))
-            %when turn slip not used: s1=1
-            %}
-            
-            FxRL(i,j)=(DXRL(i,j)*sin(CXRL(i,j)*atan(BXRL(i,j)*kxRL(i,j)-ExRL(i,j)*(BXRL(i,j)*kxRL(i,j)-atan(BXRL(i,j)*kxRL(i,j)))))+SVX)*GxalfaRL;
-            
-            
-            %REAR RIGHT
-            
-            %MF-Tyre 5.2, 6.0, 6.1
-            s1=1;
-            SHXRR(i,j)= (PHX1+PHX2*DFZRR(i,j))*LHX; 
-            
-            %Pure Slip
-            kxRR(i,j) = k(i,j)+SHXRR(i,j);
-            CXRR(i,j) = PCX1*LCX;
-            URR(i,j)  = (PDX1+PDX2*DFZRR(i,j))*(1+PPX3*DPI+PPX4*DPI^2)*(1-PDX3*CAM^2)*LMUX;
-            DXRR(i,j) = URR(i,j)*FZRR(i,j)*s1;
-            ExRR(i,j) = (PEX1+PEX2*DFZRR(i,j)+PEX3*DFZRR(i,j)^2)*(1-PEX4*sign(kxRR(i,j)))*LEX;
-            KXkRR(i,j) = FZRR(i,j)*(PKX1+PKX2*DFZRR(i,j))*exp(PKX3*DFZRR(i,j))*(1+PPX1*DPI+PPX2*DPI^2)*LKXk; 
-            BXRR(i,j) = KXkRR(i,j)/(CXRR(i,j)*DXRR(i,j));
-            %Combined slip
-            %{
-            SHalfaRR= RHX1
-            alfaSRR= alfaF+SHalfaRR
-            BXalfaRR= (RBX1+RBX3*CAM^2)*cos(arctan(RBX2*k))*LXA
-            CXalfaRR= RCX1
-            EXARR= REX1+REX2*DFZ
-            %}
-            GxalfaRR=1;
-            %{
-            GxalfaRR= (cos(CXalfaRR*arctan(BXalfaRR*alfaSRR-EXARR(BXalfaRR*alfaSRR-arctan(BXalfaRR*alfaSRR)))))/(cos(CXalfaRR*arctan(BXalfaRR*SHalfaRR-EXalfa(BXalfaRR-SHalfaRR-arctan(BXalfaRR*SHXalfaRR)))))
-            when combined slip is not used: Gxa=1
-            %}
-            %Turn slip
-            s1=1; 
-            %{
-            BXP1RR= PDXP1(1+PDXP2*DFZ)*cos(arctan(PDXP3*k))
-            s1= cos(arctan(BXP1RR*R*PF))
-            %when turn slip not used: s1=1
-            %}
-            
-            FxRR(i,j)=(DXRR(i,j)*sin(CXRR(i,j)*atan(BXRR(i,j)*kxRR(i,j)-ExRR(i,j)*(BXRR(i,j)*kxRR(i,j)-atan(BXRR(i,j)*kxRR(i,j)))))+SVX)*GxalfaRR;
-            
-            %Total
-            Fx4(i,j)=FxFL(i,j)+FxFR(i,j)+FxRL(i,j)+FxRR(i,j); %Only 4WD
-            Fx2(i,j)=FxRL(i,j)+FxRR(i,j); %2WD
-            Acx(i,j)=Fx2(i,j)/m; %Longitudinal acceleration
-            
-        end
-    
-    VmaxAccel(i,j)=sqrt(2*75*Acx(i,j))*3.6; %Maximum acceleration velocity in 75 m;
-    end
+        VmaxAccel(i)=sqrt(2*75*Acx(i))*3.6; %Maximum acceleration velocity in 75 m;
+end
+else
+    fprintf('error: length(slip)!=length(VV)');
 end
 
+  
 figure(1)
 plot3(slip,Fx2,V)
 
